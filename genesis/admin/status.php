de47b744e48974baee9d167992b687ada9fc46fe
Emmanuel05B <143185198+Emmanuel05B@users.noreply.github.com>
2024-12-29 22:11:56 +0200

    updated pages

--- genesis/admin/status.php
+++ genesis/admin/status.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html>
-<html>
-    
 <?php
 session_start();
 
@@ -13,18 +10,18 @@ if (!isset($_SESSION['email'])) {
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
   <!-- Left side column. contains the logo and sidebar -->
-  <?php include("adminpartials/header.php") ?>;
+  <?php include("adminpartials/header.php"); ?>
   <!-- Left side column. contains the logo and sidebar -->
- <?php include("adminpartials/mainsidebar.php") ?>;
+  <?php include("adminpartials/mainsidebar.php"); ?>
 
   <!-- Content Wrapper. Contains page content --->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
      
-</section>
+    </section>
 
-    <!-- Main content table--------------------------------------------->
+    <!-- Main content table---------------------------------------------> 
     <section class="content">
       <div class="row">
         <div class="col-xs-12">
@@ -35,17 +32,17 @@ if (!isset($_SESSION['email'])) {
             <div class="box-body">
               <table id="example1" class="table table-bordered table-striped">
                 <thead>
-                <tr>
-                      <th>StNo</th>
-                      <th>Name</th>
-                      <th>Surname</th>
-                      <th>Grade</th>
-                      <th>Math</th>
-                      <th>Physics</th>
-                      <th>Total Fees</th>
-                      <th>Total Paid</th>
-                      <th>Total Owe</th>
-                </tr>
+                  <tr>
+                    <th>StNo</th>
+                    <th>Name</th>
+                    <th>Surname</th>
+                    <th>Grade</th>
+                    <th>Math</th>
+                    <th>Physics</th>
+                    <th>Total Fees</th>
+                    <th>Total Paid</th>
+                    <th>Total Owe</th>
+                  </tr>
                 </thead> 
 
                 <tbody>
@@ -56,52 +53,47 @@ if (!isset($_SESSION['email'])) {
           
                 // Check the status and render different HTML for each case
                 if ($statusValue == 1) {
-                    // Status 1
+                    // Status 1: On Contract and Owing Learners
                     echo '<h3>On Contract and Owing Learners</h3><br>';
-          
-                    $sql = "SELECT lt.LearnerId, lt.TotalOwe,  
-                    ls.LearnerSubjectId,
-                    ls.SubjectId, 
-                    ls.ContractExpiryDate, 
-                    ls.Status
-                    
-                    FROM learners AS lt
-                    JOIN learnersubject AS ls ON lt.LearnerId = ls.LearnerId
-                    WHERE lt.TotalOwe => 0
-                    AND ls.ContractExpiryDate > CURDATE()";  
-          
-          
-                   // $sql = "SELECT * FROM learners WHERE TotalOwe > 0";
-                   
-          
-                } else if ($statusValue == 2) {
-                    // Status 2
-                    echo '<h3>On Contract and Not Owing Learners</h3><br>';
-          
-                    $sql = "SELECT * FROM learners WHERE TotalOwe <= 0";
- 
-          
-                } else if ($statusValue == 3) {
-                    // Status 3
-                    echo '<h3>Expired Contract and Not Owing Learners</h3><br>';
-          
-                    $sql = "SELECT * FROM learners WHERE TotalOwe <= 0";
 
-          
+                    // Correct SQL query for learners owing money and with unexpired contracts
+                   /* $sql = "SELECT lt.LearnerId, lt.Name, lt.Surname, lt.Grade, lt.TotalFees, lt.TotalPaid, lt.TotalOwe,
+                            ls.LearnerSubjectId, ls.SubjectId, ls.Math, ls.Physics, ls.ContractExpiryDate, ls.Status
+                            FROM learners AS lt
+                            JOIN learnersubject AS ls ON lt.LearnerId = ls.LearnerId
+                            WHERE lt.TotalOwe > 0
+                            AND ls.ContractExpiryDate > CURDATE()";  */
+
+                             $sql = "SELECT lt.*, ls.*
+                            FROM learners AS lt
+                            JOIN learnersubject AS ls ON lt.LearnerId = ls.LearnerId
+                            WHERE lt.TotalOwe > 0
+                            AND ls.ContractExpiryDate = (
+                                SELECT MAX(ls2.ContractExpiryDate)
+                                FROM learnersubject AS ls2
+                                WHERE ls2.LearnerId = ls.LearnerId
+                            )
+                            AND ls.ContractExpiryDate > CURDATE()";
+
+                } else if ($statusValue == 2) {
+                    // Status 2: On Contract and Not Owing Learners
+                    echo '<h3>On Contract and Not Owing Learners</h3><br>';
+                    $sql = "SELECT * FROM learners WHERE TotalOwe <= 0";
+                } else if ($statusValue == 3) {
+                    // Status 3: Expired Contract and Not Owing Learners
+                    echo '<h3>Expired Contract and Not Owing Learners</h3><br>';
+                    $sql = "SELECT * FROM learners WHERE TotalOwe <= 0";
                 } else if ($statusValue == 4) {
-                    // Status 4
+                    // Status 4: Expired Contract and Owing Learners
                     echo '<h3>Expired Contract and Owing Learners</h3><br>';
-          
                     $sql = "SELECT * FROM learners WHERE TotalOwe > 0";
-          
                 } else {
                     // Default case if none of the statuses match
                     echo '<h1>Learners - Unknown Status</h1>';
-          
                 }
                     
-                    $results = $connect->query($sql);
-                    while($final = $results->fetch_assoc()) { ?> 
+                $results = $connect->query($sql);
+                while($final = $results->fetch_assoc()) { ?> 
                 <tr>
                     <td><?php echo $final['LearnerId'] ?></td>
                     <td><?php echo $final['Name'] ?></td>
@@ -112,22 +104,21 @@ if (!isset($_SESSION['email'])) {
                     <td><?php echo $final['TotalFees'] ?></td>
                     <td><?php echo $final['TotalPaid'] ?></td>
                     <td> <?php echo $final['TotalOwe'] ?></td>
-
                 </tr>
                 <?php } ?>
                 </tbody>
                 <tfoot>
-                <tr>
-                <th>StNo</th>
-                      <th>Name</th>
-                      <th>Surname</th>
-                      <th>Grade</th>
-                      <th>Math</th>
-                      <th>Physics</th>
-                      <th>Total Fees</th>
-                      <th>Total Paid</th>
-                      <th>Total Owe</th>
-                </tr>
+                  <tr>
+                    <th>StNo</th>
+                    <th>Name</th>
+                    <th>Surname</th>
+                    <th>Grade</th>
+                    <th>Math</th>
+                    <th>Physics</th>
+                    <th>Total Fees</th>
+                    <th>Total Paid</th>
+                    <th>Total Owe</th>
+                  </tr>
                 </tfoot>
               </table>
             </div>
@@ -139,233 +130,24 @@ if (!isset($_SESSION['email'])) {
       </div>
       <!-- /.row -->
     </section>
- 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Create the tabs -->
-    <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-      <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-      <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-    </ul>
-    <!-- Tab panes -->
-    <div class="tab-content">
-      <!-- Home tab content -->
-      <div class="tab-pane" id="control-sidebar-home-tab">
-        <h3 class="control-sidebar-heading">Recent Activity</h3>
-        <ul class="control-sidebar-menu">
-          <li>
-            <a href="javascript:void(0)">
-              <i class="menu-icon fa fa-birthday-cake bg-red"></i>
-
-              <div class="menu-info">
-                <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
-
-                <p>Will be 23 on April 24th</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="javascript:void(0)">
-              <i class="menu-icon fa fa-user bg-yellow"></i>
-
-              <div class="menu-info">
-                <h4 class="control-sidebar-subheading">Frodo Updated His Profile</h4>
-
-                <p>New phone +1(800)555-1234</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="javascript:void(0)">
-              <i class="menu-icon fa fa-envelope-o bg-light-blue"></i>
-
-              <div class="menu-info">
-                <h4 class="control-sidebar-subheading">Nora Joined Mailing List</h4>
-
-                <p>nora@example.com</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="javascript:void(0)">
-              <i class="menu-icon fa fa-file-code-o bg-green"></i>
-
-              <div class="menu-info">
-                <h4 class="control-sidebar-subheading">Cron Job 254 Executed</h4>
-
-                <p>Execution time 5 seconds</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <!-- /.control-sidebar-menu -->
-
-        <h3 class="control-sidebar-heading">Tasks Progress</h3>
-        <ul class="control-sidebar-menu">
-          <li>
-            <a href="javascript:void(0)">
-              <h4 class="control-sidebar-subheading">
-                Custom Template Design
-                <span class="label label-danger pull-right">70%</span>
-              </h4>
-
-              <div class="progress progress-xxs">
-                <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="javascript:void(0)">
-              <h4 class="control-sidebar-subheading">
-                Update Resume
-                <span class="label label-success pull-right">95%</span>
-              </h4>
-
-              <div class="progress progress-xxs">
-                <div class="progress-bar progress-bar-success" style="width: 95%"></div>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="javascript:void(0)">
-              <h4 class="control-sidebar-subheading">
-                Laravel Integration
-                <span class="label label-warning pull-right">50%</span>
-              </h4>
-
-              <div class="progress progress-xxs">
-                <div class="progress-bar progress-bar-warning" style="width: 50%"></div>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="javascript:void(0)">
-              <h4 class="control-sidebar-subheading">
-                Back End Framework
-                <span class="label label-primary pull-right">68%</span>
-              </h4>
-
-              <div class="progress progress-xxs">
-                <div class="progress-bar progress-bar-primary" style="width: 68%"></div>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <!-- /.control-sidebar-menu -->
-
-      </div>
-      <!-- /.tab-pane -->
-      <!-- Stats tab content -->
-      <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
-      <!-- /.tab-pane -->
-      <!-- Settings tab content -->
-      <div class="tab-pane" id="control-sidebar-settings-tab">
-        <form method="post">
-          <h3 class="control-sidebar-heading">General Settings</h3>
-
-          <div class="form-group">
-            <label class="control-sidebar-subheading">
-              Report panel usage
-              <input type="checkbox" class="pull-right" checked>
-            </label>
-
-            <p>
-              Some information about this general settings option
-            </p>
-          </div>
-          <!-- /.form-group -->
-
-          <div class="form-group">
-            <label class="control-sidebar-subheading">
-              Allow mail redirect
-              <input type="checkbox" class="pull-right" checked>
-            </label>
-
-            <p>
-              Other sets of options are available
-            </p>
-          </div>
-          <!-- /.form-group -->
-
-          <div class="form-group">
-            <label class="control-sidebar-subheading">
-              Expose author name in posts
-              <input type="checkbox" class="pull-right" checked>
-            </label>
-
-            <p>
-              Allow the user to show his name in blog posts
-            </p>
-          </div>
-          <!-- /.form-group -->
-
-          <h3 class="control-sidebar-heading">Chat Settings</h3>
-
-          <div class="form-group">
-            <label class="control-sidebar-subheading">
-              Show me as online
-              <input type="checkbox" class="pull-right" checked>
-            </label>
-          </div>
-          <!-- /.form-group -->
-
-          <div class="form-group">
-            <label class="control-sidebar-subheading">
-              Turn off notifications
-              <input type="checkbox" class="pull-right">
-            </label>
-          </div>
-          <!-- /.form-group -->
-
-          <div class="form-group">
-            <label class="control-sidebar-subheading">
-              Delete chat history
-              <a href="javascript:void(0)" class="text-red pull-right"><i class="fa fa-trash-o"></i></a>
-            </label>
-          </div>
-          <!-- /.form-group -->
-        </form>
-      </div>
-      <!-- /.tab-pane -->
-    </div>
-  </aside>
-  <div class="control-sidebar-bg"></div>
+  </div>
 </div>
-<!-- ./wrapper -->
+<!-- End of wrapper -->
 
-
-<!-- jQuery 3 -->
+<!-- Scripts -->
 <script src="bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
 <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- DataTables -->
 <script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-<!-- SlimScroll -->
 <script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
 <script src="bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
-
-
-<!-- page script -->
 <script>
   $(function () {
-    $('#example1').DataTable()
-    $('#example2').DataTable({
-      'paging'      : true,
-      'lengthChange': false,
-      'searching'   : false,
-      'ordering'    : true,
-      'info'        : true,
-      'autoWidth'   : false
-    })
-  })
+    $('#example1').DataTable();
+  });
 </script>
-
 
 </body>
 </html>
